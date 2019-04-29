f3c44366a85aa5061d44c2541b3cdc197674bd5a
Ronald Cho <ronaldcho@gmail.com>
2019-04-29 12:08:37 -0700

    functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,19 +1,34 @@
 // ==== Callbacks ====  
 
 /* Step 1: Create a higher-order function that accepts a callback
-  * Create a higher-order function named consume that can take 3 parameters.
-  * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
-*/
-
+ * Create a higher-order function named consume that can take 3 parameters.
+ * The first two parameters can accept any argument
+ * The last parameter accepts a callback 
+ * In the body of the function return the callback with the two parameters that you created
+ */
+function consume(par1, par2, cb) {
+  return cb(par1, par2);
+}
 
 /* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers 
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
+const add = (a, b) => a + b;
+console.log(consume(5, 8, add));
 
+const multiply = (a, b) => a * b;
+console.log(consume(9, 5, multiply));
+
+const greeting = (first, last) => `Hello ${first} ${last}, nice to meet you!`;
+console.log(consume('Donald', 'Trump', greeting));
+
+/* Step 3: Check your work by un-commenting the following calls to consume(): */
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log("#############################################################\n");
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
@@ -36,7 +51,9 @@ function myFunction() {
 
   function nestedFunction() {
     console.log(internal);
+    console.log(`after the internal print out "${external}"`);
   };
-  nestedFunction();
+  nestedFunction(); // without call nestedFunction only myFunction run
+  console.log(`nestedFunciton called inside the myFunction!!!!`);
 }
 myFunction();
